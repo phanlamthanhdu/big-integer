3fc6b740e862badd4310adbadf633462ca358909
phanlamthanhdu <phanlamthanhdu@gmail.com>
2023-09-03 18:13:23 +0700

    Modify the big_integer files

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,24 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-//MAX_LONG_LONG = 9 223 372 036 854 775 807
+// MAX_LONG_LONG = 9 223 372 036 854 775 807
 
-int lower_fence = 100'000'000'000'000'000;
+long long lower_fence = (long long) 1e17;
 struct big_integer{
+    /* 
+        Using a vector as a big_integer data structure
+        Fast and Easy storage
+        Each slot in the vector stores 17 digits
+        Ex:     13 40004323342315782 
+            When storing in my big_integer, it'll be reversed:
+                40004323342315782 13
+                
+    */ 
     vector <long long> data;
 
+    // Default Constructor
     big_integer(){}
+    // Parameterized Constructor
     big_integer(int x){
         data.push_back(x);
     }
+
+    // Operator Overloading
+    // Opertor =
     void operator=(big_integer x){
-        data.clear();
-        for(auto v : x.data) data.push_back(v);
+        data.clear();                                   // Removing all the digits before assigning 
+        for(auto v : x.data) data.push_back(v);    // Push each slot from x into a new big_integer
     }
+
+    // Operator +
     big_integer operator+(const big_integer x) const{
         big_integer ans;
         int carry{0},i{0};
 
-        while(i < x.data.size()){
+        while(i < x.data.size() && i < data.size()){
             long long res = data[i] + x.data[i] + carry;
             ans.data.push_back(res % lower_fence);
             carry = res / lower_fence;
@@ -30,20 +46,28 @@ struct big_integer{
             carry = res / lower_fence;
             i++;
         }
+        while(i < x.data.size()){
+            long long res = x.data[i] + carry;
+            ans.data.push_back(res % lower_fence);
+            carry = res / lower_fence;
+            i++;
+        }
         if(carry) ans.data.push_back(carry);
         return ans;
     }
+
+    // Print the big_integer
     void output(){
         for(int i(data.size()-1); i >= 0; i--){
-            if(data[i] < lower_fence && i != data.size()-1)
-                for(int j{0}; j < 16-(int)log10(data[i]); j++) 
-                    cout << 0;
-            cout << data[i];
+            if(i != data.size()-1) cout << setw(17) << setfill('0') << data[i]; // Printing without digit-losing issue
+            else cout << data[i];
         }
     }
 };
 
 int main(){
+
+    // Implement big_integer with Fibonacci
 	big_integer f1 = 1, f2 = 1;
 	int n;
 	cin >> n;
--- lychrel_number.cpp
+++ lychrel_number.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long lower_fence = (long long) 1e17;
 struct big_integer{
     vector <long long> data;
 
     big_integer(){}
     big_integer(string &s){
+
         int n(s.length()-1), cnt{0};
         string res;
         for(int i{n}; i >= 0; i--){
             res += s[i];
             cnt++;
-            if(cnt == 18 || i == 0){
+            if(cnt == 17 || i == 0){
                 reverse(res.begin(),res.end());
-                data.push_back(strtoll(res.c_str(),nullptr,10));
+                data.push_back(strtoll(res.c_str(), nullptr, 10));
                 res.clear();
                 cnt = 0;
             }
@@ -21,13 +23,12 @@ struct big_integer{
     }
 
     big_integer get_reverse(){        
-
         //Convert big_integer to string
         string s;
 
         for(int i(data.size()-1); i >= 0; i--){
-            if(data[i] < 100'000'000'000'000'000 && i != data.size()-1)
-                for(int j{0}; j < 17-(int)log10(data[i]+0.9); j++) 
+            if(data[i] < lower_fence && i != data.size()-1)
+                for(int j{0}; j < 16-(int)log10(data[i]+0.9); j++) 
                     s += '0';
             s += to_string(data[i]);
         }
@@ -37,31 +38,27 @@ struct big_integer{
         big_integer ans(s);
         return ans;
     }
-
     int length(){
         int n(data.size());
         int count = to_string(data[n-1]).length();
         if(n > 1) count += (n-1) * 18;
         return count;
     }
-
     big_integer& operator=(const big_integer &x){
         data.clear();
         for(auto v : x.data) data.push_back(v);
         return *this;
     }
-
     bool operator==(const big_integer &x) const{
-        int i(data.size()-1);
-        //     j(x.data.size()-1);
-        // if(i != j) return 0;
+        int i(data.size()-1),
+            j(x.data.size()-1);
+        if(i != j) return 0;
         while(i >= 0){
-            if(data[i] != x.data[i]) return 0;
+            if(data[i] != x.data[j]) return 0;
             i--;
         }
         return 1;
     }
-
     big_integer operator+(const big_integer &x) const{
         big_integer ans;
         long long carry{0};
@@ -69,44 +66,42 @@ struct big_integer{
 
         while(i < x.data.size() && i < data.size()){
             long long res = data[i] + x.data[i] + carry;
-            ans.data.push_back(res % 1'000'000'000'000'000'000);
-            carry = res / 1'000'000'000'000'000'000;
-            i++;
-        }
-        while(i < x.data.size()){
-            long long res = x.data[i] + carry;
-            ans.data.push_back(res % 100'000'000'000'000'000);
-            carry = res / 100'000'000'000'000'000;
+            ans.data.push_back(res % lower_fence);
+            carry = res / lower_fence;
             i++;
         }
         while(i < data.size()){
             long long res = data[i] + carry;
-            ans.data.push_back(res % 100'000'000'000'000'000);
-            carry = res / 100'000'000'000'000'000;
+            ans.data.push_back(res % lower_fence);
+            carry = res / lower_fence;
+            i++;
+        }
+        while(i < x.data.size()){
+            long long res = x.data[i] + carry;
+            ans.data.push_back(res % lower_fence);
+            carry = res / lower_fence;
             i++;
         }
         if(carry) ans.data.push_back(carry);
         return ans;
     }
-
     void output(){
         for(int i(data.size()-1); i >= 0; i--){
-            if(i != data.size()-1) cout << setw(18) << setfill('0') << data[i];
+            if(i != data.size()-1) cout << setw(17) << setfill('0') << data[i];
             else cout << data[i];
         }
     }
 };
 
 int main(){
-	int n;
-    int i = 0;
+	int n, i = 0;
     int limit_rep = 1e4, limit_len = 1.5e4;
 
     string input;
     cin >> input;
-    
-    auto s = big_integer(input);
-    auto rs = s.get_reverse();
+
+    big_integer s = big_integer(input);
+    big_integer rs = s.get_reverse();
 
     vector<big_integer> results;
     while(i <= limit_rep and s.length() <= limit_len){   
@@ -116,7 +111,7 @@ int main(){
         results.push_back(s);
         if (s == rs){
             cout << "NO" << endl; 
-            for(auto i : results){
+            for(big_integer i : results){
                 i.output();
                 cout << endl;
             }
